a3db3ae77134f168bb0de2aa14369c750fbd3055
CharlesKJunior <mutebwacharles@gmail.com>
2023-10-31 17:37:30 +0300

    course work

---
+++ app.js
@@ -0,0 +1,10 @@
+let inputbox=document.getElementById('inputbox')
+let buttons =document.querySelectorAll('button')
+
+let string=''
+
+buttons.forEach(Element=>{
+    Element.addEventListener('click',(b)=>{
+        if
+    })
+})
